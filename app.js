de0742b960da31ea30cc95f57a5f846de9178732
Avishkar <avishkar1234567890@gmail.com>
2026-01-26 19:24:40 +0530

    Complete Mini Message Board project with Express and EJS

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const indexRouter = require("./routes/index")
+
+const app = express()
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/", indexRouter)
+
+app.listen(3000, () => {
+    console.log(`Server is running on http:localhost:3000`)
+})
